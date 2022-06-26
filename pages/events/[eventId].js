799669cb2fcf96cade244b53d061d54cc7273681
Mahdi A <mahdi.alizadeh.sp@gmail.com>
2022-06-26 14:25:25 +0430

    API routes start project

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,27 +1,33 @@
-import { getAllEvents, getEventById } from '../../helpers/api-utils';
-
-import ErrorAlert from '../../components/ui/error-alert';
-import EventContent from '../../components/event-detail/event-content';
-import EventLogistics from '../../components/event-detail/event-logistics';
-import EventSummary from '../../components/event-detail/event-summary';
 import { Fragment } from 'react';
+import Head from 'next/head';
 
-function EventDetailPage({selectedEvent ,id}) {
+import { getEventById, getFeaturedEvents } from '../../helpers/api-util';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+import ErrorAlert from '../../components/ui/error-alert';
+import Comments from '../../components/input/comments';
 
-  console.log(id);
-
-  const event = selectedEvent;
+function EventDetailPage(props) {
+  const event = props.selectedEvent;
 
   if (!event) {
     return (
-      <ErrorAlert>
-        <p>No event found!</p>
-      </ErrorAlert>
+      <div className="center">
+        <p>Loading...</p>
+      </div>
     );
   }
 
   return (
     <Fragment>
+      <Head>
+        <title>{event.title}</title>
+        <meta
+          name='description'
+          content={event.description}
+        />
+      </Head>
       <EventSummary title={event.title} />
       <EventLogistics
         date={event.date}
@@ -32,34 +38,33 @@ function EventDetailPage({selectedEvent ,id}) {
       <EventContent>
         <p>{event.description}</p>
       </EventContent>
+      <Comments eventId={event.id} />
     </Fragment>
   );
-};
+}
 
 export async function getStaticProps(context) {
-
   const eventId = context.params.eventId;
 
   const event = await getEventById(eventId);
 
   return {
-    props : {
-      selectedEvent : event,
-      id : eventId
-    }
-  }
-};
+    props: {
+      selectedEvent: event
+    },
+    revalidate: 30
+  };
+}
 
 export async function getStaticPaths() {
+  const events = await getFeaturedEvents();
 
-  const allEvents = await getAllEvents();
-
-  const paths = allEvents.map((item) => ({params : { eventId : item.id }}))
+  const paths = events.map(event => ({ params: { eventId: event.id } }));
 
   return {
-    paths : paths,
-    fallback : false
-  }
+    paths: paths,
+    fallback: 'blocking'
+  };
 }
 
 export default EventDetailPage;
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,34 @@
-import { getAllEvents, getFeaturedEvents } from '../helpers/api-utils';
+import Head from 'next/head';
 
+import { getFeaturedEvents } from '../helpers/api-util';
 import EventList from '../components/events/event-list';
+import NewsletterRegistration from '../components/input/newsletter-registration';
 
-function HomePage({events}) {
-
+function HomePage(props) {
   return (
     <div>
-      <EventList items={events} />
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name='description'
+          content='Find a lot of great events that allow you to evolve...'
+        />
+      </Head>
+      <NewsletterRegistration />
+      <EventList items={props.events} />
     </div>
   );
-  
-};
-
+}
 
 export async function getStaticProps() {
-
   const featuredEvents = await getFeaturedEvents();
 
   return {
-    props : {
-      events : featuredEvents
-    }
-  }
-};
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 1800,
+  };
+}
 
 export default HomePage;
